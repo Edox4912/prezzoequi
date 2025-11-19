71532e575b08d820cbb6552c26455a31f21c613c
PESENTI.21227 <pesenti.edoardo.studente@itispaleocapa.it>
2025-11-19 13:48:51 +0100

    primo commit, tanti bug

--- prezzoequi/Form1.Designer.cs
+++ prezzoequi/Form1.Designer.cs
@@ -28,20 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tb_q = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_insert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(524, 69);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(337, 350);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // tb_q
+            // 
+            this.tb_q.Location = new System.Drawing.Point(263, 69);
+            this.tb_q.Name = "tb_q";
+            this.tb_q.Size = new System.Drawing.Size(101, 20);
+            this.tb_q.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "inserisci q";
+            // 
+            // btn_insert
+            // 
+            this.btn_insert.Location = new System.Drawing.Point(109, 110);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(255, 74);
+            this.btn_insert.TabIndex = 3;
+            this.btn_insert.Text = "inserisci";
+            this.btn_insert.UseVisualStyleBackColor = true;
+            this.btn_insert.Click += new System.EventHandler(this.btn_insert_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(913, 528);
+            this.Controls.Add(this.btn_insert);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_q);
+            this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox tb_q;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_insert;
     }
 }
 
--- prezzoequi/Form1.cs
+++ prezzoequi/Form1.cs
@@ -16,5 +16,52 @@ namespace prezzoequi
         {
             InitializeComponent();
         }
+
+        private void btn_insert_Click(object sender, EventArgs e)
+        {
+            private (double Quantita, double Prezzo) TrovaEquilibrio(
+            double domandaInt, double domandaCoeff,
+            double offertaInt, double offertaCoeff, double offertaEsp)
+        {
+            double qMin = 0;
+            double qMax = 20;
+            double tolleranza = 0.001;
+            int maxIterazioni = 1000;
+
+            for (int i = 0; i < maxIterazioni; i++)
+            {
+                double qMedio = (qMin + qMax) / 2;
+                double differenza = CalcolaDifferenza(qMedio, domandaInt, domandaCoeff, offertaInt, offertaCoeff, offertaEsp);
+
+                if (Math.Abs(differenza) < tolleranza)
+                {
+                    double prezzo = domandaInt - domandaCoeff * qMedio;
+                    return (qMedio, prezzo);
+                }
+
+                double diffMin = CalcolaDifferenza(qMin, domandaInt, domandaCoeff, offertaInt, offertaCoeff, offertaEsp);
+
+                if (diffMin * differenza < 0)
+                    qMax = qMedio;
+                else
+                    qMin = qMedio;
+            }
+
+            double qFinale = (qMin + qMax) / 2;
+            double pFinale = domandaInt - domandaCoeff * qFinale;
+            return (qFinale, pFinale);
+        }
+
+        private double CalcolaDifferenza(double q, double domandaInt, double domandaCoeff,
+                                 double offertaInt, double offertaCoeff, double offertaEsp)
+        {
+            double prezzoDomanda = domandaInt - domandaCoeff * q;
+            double prezzoOfferta = offertaInt + offertaCoeff * Math.Pow(q, offertaEsp);
+            return prezzoDomanda - prezzoOfferta;
+        }
+    
+
     }
 }
+    
+
